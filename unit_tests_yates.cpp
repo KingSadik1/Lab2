1f6924d188e2db0671902fe08086d27172b138b7
King Sadik <123670994+KingSadik1@users.noreply.github.com>
2023-02-05 15:13:07 -0500

    Update unit_tests_yates.cpp

--- unit_tests_yates.cpp
+++ unit_tests_yates.cpp
@@ -4,9 +4,9 @@
  */
 
 #include "doctest.h"
-#include "prefix_sum.h"
+#include "scrambler.h"
 
-/*int nums_0[] = {};
+int nums_0[] = {};
 int nums_1[] = {-5};
 int nums_2[] = {35};
 int nums_3[] = {2,-3,3,2};
@@ -20,7 +20,7 @@ TEST_CASE("Test week1/2 functions") {
     CHECK(non_negative_prefix_sum(nums_0, 0)== true);
     CHECK(non_negative_prefix_sum(nums_2, 1)== true);
     CHECK(non_negative_prefix_sum(nums_4, 4)== true);
-    CHECK(non_positive_prefix_sum(nums_0, 0)== false);
+    CHECK(non_positive_prefix_sum(nums_0, 0)== true);
     CHECK(non_positive_prefix_sum(nums_2, 1)== false);
     CHECK(non_positive_prefix_sum(nums_4, 4)== false);
 
@@ -29,9 +29,9 @@ TEST_CASE("Test week1/2 functions") {
     CHECK(non_negative_prefix_sum(nums_1, 1)== false);
     CHECK(non_negative_prefix_sum(nums_3, 4)== false);
     CHECK(non_negative_prefix_sum(nums_5, 6)== false);
-    CHECK(non_negative_prefix_sum(nums_1, 1)== true);
-    CHECK(non_negative_prefix_sum(nums_3, 4)== true);
-    CHECK(non_negative_prefix_sum(nums_5, 6)== true);
+    CHECK(non_positive_prefix_sum(nums_1, 1)== true);
+    CHECK(non_positive_prefix_sum(nums_3, 4)== false);
+    CHECK(non_positive_prefix_sum(nums_5, 6)== true);
   };
 }*/
 
